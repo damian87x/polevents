3592a3f7fa4b7014ab56571ff8239d6236c44c8b
damian87x <borekdamian@yahoo.pl>
2016-07-16 19:07:57 +0100

    Add id tests for user and one red for id validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,26 +2,41 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let(:user) { FactoryGirl.create(:user) }
 
-  describe 'UUID' do
+  describe 'ID' do
 
-    it 'there should be an UUID generated if creating new users' do
+
+    describe 'when create' do
+      let(:user_one) { FactoryGirl.create(:user) }
+      let(:user_two) { FactoryGirl.create(:user) }
+
+      it 'there should be an ID different if creating new users' do
+        expect(user_one.id != user_two.id).to be_truthy
+      end
+
+      it 'there should be an ID not blank for users' do
+        expect(user_one.id && user_two.id).to be_truthy
+      end
+
 
     end
 
-    it 'there is no UUID generation for a user with UUID set during creation' do
+
+    describe 'when update' do
+      let(:user_update) { FactoryGirl.create(:user) }
+
+
+      it 'once ID is set, you cannot update it' do
+        user_update.update_attributes(id: SecureRandom.uuid)
+        expect(user_update).to be_invalid
+        expect(user_update.errors[:id]).to include("id can't be updated")
+      end
+
 
     end
-
-    it 'once UUID is set, you cannot update it' do
-
-    end
-
 
 
   end
-
 
 
 end
