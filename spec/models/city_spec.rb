82ddb3d2267fe330bb01aea82f6a2370be212e26
damian87x <borekdamian@yahoo.pl>
2016-07-16 21:22:02 +0100

    Add relation tests for city and topic

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  describe 'should have relations' do
+
+    it { should have_many(:events) }
+
+
+  end
+
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'should have relations' do
+
+    it { should have_many(:events) }
+
+
+  end
 end
