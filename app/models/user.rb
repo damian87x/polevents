35883e752b133904b77ac69687357b533e2b2761
damian87x <borekdamian@yahoo.pl>
2016-07-16 19:12:40 +0100

    Add id validation to model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  validate :id_not_changed
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
+  def id_not_changed
+    if self.id_changed? and self.persisted?
+      self.errors.add(:id, "id can't be updated")
+    end
+  end
 
 
 end
