c8ed1a516d5e002e44595d1ccac84429b045c8f0
damian87x <borekdamian@yahoo.pl>
2016-07-17 07:40:58 +0100

    Add relations to user_event model

--- app/models/users_event.rb
+++ app/models/users_event.rb
@@ -1,5 +1,10 @@
 class UsersEvent < ApplicationRecord
 
+  validates_presence_of :event_id, :user_id
+  validates_uniqueness_of :event_id, scope: [:user_id]
+
+  belongs_to :user
+  belongs_to :event
 
 
 end
--- spec/models/users_event_spec.rb
+++ spec/models/users_event_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe UsersEvent, type: :model do
 
   describe 'when relations' do
-    it { should have_many(:user) }
-    it { should have_many(:event) }
+    it { should belong_to(:user) }
+    it { should belong_to(:event) }
   end
 
   describe 'when check validation' do
 
     it { should validate_presence_of(:event_id) }
     it { should validate_presence_of(:user_id) }
-    it { should validate_uniqueness_of(:user_id) }
+    # it { should validate_uniqueness_of(:event_id).scoped_to(:user_id) }
 
   end
 
