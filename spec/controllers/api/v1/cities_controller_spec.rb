57c0439f8b0689f41753a8f528f870bdfc33fe8f
damian87x <borekdamian@yahoo.pl>
2016-07-18 13:19:59 +0100

    Create cites api controller and tests

---
+++ spec/controllers/api/v1/cities_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::CitiesController, type: :controller do
+
+  describe "GET #index" do
+    before do
+      3.times do
+        FactoryGirl.create(:city)
+      end
+      get :index
+      @json = JSON.parse(response.body)
+    end
+
+    it "should returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return cites" do
+      expect(@json['cites']).to be_truthy
+    end
+
+    it "should return cites" do
+      expect(@json['cites'].count).to eq(3)
+    end
+
+  end
+
+end
