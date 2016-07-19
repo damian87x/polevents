0abf4d62e6604a68543ea42e00938db88abbe42b
damian87x <borekdamian@yahoo.pl>
2016-07-19 14:18:21 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,32 @@ Topic.create([
               {name: 'Programmer no live'},
              ])
 
+
+DateAndRange.create(
+        [
+                {start_time: Time.now,end_time: Time.now + 2.hours, start_date: Time.now},
+                {start_time: Time.now,end_time: Time.now + 1.hours, start_date: Time.now}
+
+        ]
+)
+
+
 User.create([
         {email: 'test@test.uk', password: '12345678'},
         {email: 'test@test2.uk', password: '12345678'}
             ])
+
+Event.create([
+                     {name: 'Programming',start_time: Time.now, end_time: Time.now + 1.hours,
+                      user_id: User.first.id, city_id: City.first.id, topic_id: Topic.first.id  },
+                     {name: 'React JS', start_time: Time.now, end_time: Time.now + 2.hours,
+                      user_id: User.last.id, city_id: City.last.id, topic_id: Topic.last.id }
+             ])
+
+
+Filter.create([
+               {user_id: User.first.id, owner_id: City.last.id, owner_type: 'City'},
+               {user_id: User.first.id, owner_id: Topic.last.id, owner_type: 'Topic'},
+               {user_id: User.first.id, owner_id: Topic.last.id, owner_type: 'Topic'}
+              ])
 
